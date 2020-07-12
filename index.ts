cdf6e9a698dc69b7abab2c2df618efa81cb5c048
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-07-12 11:08:56 +0100

    feat(branches): release from the branch "main" as well as "master"
    We are working towards removing use of terms like "master". To this end we will be renaming our
    "master" branches to "main", and releases will be performed from the "main" branch. For backwards
    compatibility we will still release from "master" for repositories that have not yet been updated.
    Once the transition is complete we will only perform releases from the "main" branch.

--- index.ts
+++ index.ts
@@ -1,6 +1,20 @@
 import {Options} from "semantic-release";
 
 const options: Options = {
+    branches: [
+        "+([0-9])?(.{+([0-9]),x}).x",
+        "{main,master}",
+        "next",
+        "next-major",
+        {
+            name: "beta",
+            prerelease: true as any // TODO: Fix when types are updated
+        },
+        {
+            name: "alpha",
+            prerelease: true as any // TODO: Fix when types are updated
+        }
+    ],
     releaseRules: [
         {type: "docs", release: "patch"},
         {type: "refactor", release: "patch"}
