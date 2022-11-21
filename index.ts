ebbb1de527e9232c0e722d49f02ee22dd49b1ab4
Daniel Cassidy <mail@danielcassidy.me.uk>
2022-11-21 12:38:09 +0000

    feat(release-notes): include documentation updates and refactors in release notes

--- index.ts
+++ index.ts
@@ -20,7 +20,23 @@ const options: Options = {
             preset: "conventionalcommits"
         }],
         ["@semantic-release/release-notes-generator", {
-            preset: "conventionalcommits"
+            preset: "conventionalcommits",
+            writerOpts: {
+                types: [
+                    { type: 'feat', section: 'Features' },
+                    { type: 'feature', section: 'Features' },
+                    { type: 'fix', section: 'Bug Fixes' },
+                    { type: 'perf', section: 'Performance Improvements' },
+                    { type: 'revert', section: 'Reverts' },
+                    { type: 'docs', section: 'Documentation' },
+                    { type: 'style', section: 'Styles', hidden: true },
+                    { type: 'chore', section: 'Miscellaneous Chores', hidden: true },
+                    { type: 'refactor', section: 'Code Refactoring' },
+                    { type: 'test', section: 'Tests', hidden: true },
+                    { type: 'build', section: 'Build System', hidden: true },
+                    { type: 'ci', section: 'Continuous Integration', hidden: true }
+                ]
+            }
         }],
         "@semantic-release/npm",
         "@semantic-release/github"
